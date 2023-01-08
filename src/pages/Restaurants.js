e3e7084d881c627684b21ac696d6deeb0221c9f1
ipmanlk <io@navinda.xyz>
2023-01-08 22:05:52 +0530

    feat: axios and restaurants route

---
+++ src/pages/Restaurants.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { CartProvider } from "../context/CartContext";
+import Cart from "./components/Brand/Cart/Cart";
+import Header from "./components/Brand/Header/Header.js";
+import Restaurants from "./components/Brand/Restaurant/Restaurants/Restaurants";
+
+const Restaurants = () => {
+  return (
+    <CartProvider>
+      <Cart></Cart>
+      <Header></Header>
+      <Restaurants></Restaurants>
+    </CartProvider>
+  );
+};
+
+export default Restaurants;
--- src/pages/components/Brand/Restaurant/Restaurants/Restaurants.js
+++ src/pages/components/Brand/Restaurant/Restaurants/Restaurants.js
@@ -1,43 +1,35 @@
-import  Card  from "../../../UI/Card/Card.js";
-import RestaurantItem from "../RestaurantItem/RestaurantItem"
+import Card from "../../../UI/Card/Card.js";
+import RestaurantItem from "../RestaurantItem/RestaurantItem";
 import classes from "./Restaurants.module.scss";
 import RestaurantsSummary from "../../Restaurant/RestaurantsSummary/RestaurantsSummary";
+import { useEffect } from "react";
+import API from "../../../../../util/API.js";
 
 export default function Restaurants(props) {
+  const [restaurants, setRestaurants] = useState([]);
 
-  const isLoading = false
+  const getRestaurants = async () => {
+    try {
+      const response = await API.get("/restaurant/getrestaurants");
+      setRestaurants(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-  const restaurants = [
-    {
-      id: "1",
-      name: "Oak Ray Flower Drum Restaurant",
-      description:"Chinese, Asian, Fusion",
-      address: "38 Thurstan Road Colombo 03, Colombo 00300 Sri Lanka",
-    },
-    {
-      id: "2",
-      name: "Isso",
-      description:"Seafood, Asian, Sri Lankan",
-      address: "2 Sulaiman Terrace Green Path, Colombo 00500 Sri Lanka",
-    },
-    {
-      id: "3",
-      name: "Dolce Italia",
-      description: "ItalianCafeTuscan",
-      address: "5 Skelton Road, Colombo 00400 Sri Lanka",
-    },
-  ];
-  
+  useEffect(() => {
+    getRestaurants();
+  }, []);
 
   return (
     <>
-      <RestaurantsSummary/>  
+      <RestaurantsSummary />
       <Card className={classes.restaurants}>
         <ul>
           {isLoading && <li>Loading Restaurants, Please wait... </li>}
           {!isLoading &&
             Object.entries(restaurants).map(([key, value]) => (
-              <RestaurantItem key={key} id={key} {...value} ></RestaurantItem>
+              <RestaurantItem key={key} id={key} {...value}></RestaurantItem>
             ))}
         </ul>
       </Card>
